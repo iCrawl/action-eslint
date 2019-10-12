75265c6f6decde7b09df05f7761061d4e39cc3e5
iCrawl <icrawltogo@gmail.com>
2019-10-12 23:52:22 +0200

    refactor: use problem matchers for github actions

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { debug, getInput, setFailed, warning } from '@actions/core';
 import { context, GitHub } from '@actions/github';
-import { ChecksCreateParams, ChecksUpdateParamsOutputAnnotations } from '@octokit/rest';
 import { extname, join } from 'path';
+import * as table from 'text-table';
 
-const { GITHUB_TOKEN, GITHUB_SHA, GITHUB_WORKSPACE } = process.env;
+const { GITHUB_TOKEN, GITHUB_SHA } = process.env;
 
-const ACTION_NAME = 'ESLint';
 const EXTENSIONS = new Set(['.ts', '.js', '.tsx', '.jsx']);
 
-async function lint(files: string[] | undefined, lintAll?: string, customGlob?: string) {
+async function lint(files?: string[], lintAll?: string, customGlob?: string) {
 	const { CLIEngine } = (await import(join(process.cwd(), 'node_modules/eslint'))) as typeof import('eslint');
 	const cli = new CLIEngine({
 		extensions: [...EXTENSIONS],
@@ -23,54 +22,47 @@ async function lint(files: string[] | undefined, lintAll?: string, customGlob?: 
 	}
 	const report = cli.executeOnFiles(filesToLint);
 	const { results, errorCount, warningCount } = report;
-	const levels: ChecksUpdateParamsOutputAnnotations['annotation_level'][] = ['notice', 'warning', 'failure'];
-	const annotations: ChecksUpdateParamsOutputAnnotations[] = [];
 	const output = new Map<string, string[]>();
 	const consoleLevels = [, 'warning', 'error'];
 	for (const res of results) {
 		const { filePath, messages } = res;
-		const path = filePath.substring(GITHUB_WORKSPACE!.length + 1);
 		for (const msg of messages) {
-			const { line, endLine, column, endColumn, severity, ruleId, message } = msg;
-			const annotationLevel = levels[severity];
+			const { line, column, severity, ruleId, message } = msg;
 			const consoleLevel = consoleLevels[severity];
-			annotations.push({
-				path,
-				start_line: line,
-				end_line: endLine || line,
-				start_column: column,
-				end_column: endColumn || column,
-				annotation_level: annotationLevel,
-				title: ruleId || ACTION_NAME,
-				message: `${message}${ruleId ? `\nhttps://eslint.org/docs/rules/${ruleId}` : ''}`,
-			});
-			const hasPath = output.get(path);
+			const hasPath = output.get(filePath);
 			if (hasPath) {
-				hasPath.push(`##[${consoleLevel}]  ${line}:${column}  ${consoleLevel}  ${message}  ${ruleId}\n`);
-				output.set(path, hasPath);
+				hasPath.push(
+					`  ${line}:${column}  ${consoleLevel}  ${message} ${
+						ruleId ? `https://eslint.org/docs/rules/${ruleId}` : ''
+					}  ${ruleId || ''}\n`,
+				);
+				output.set(filePath, hasPath);
 			} else {
-				output.set(path, [
-					`${path}\n`,
-					`##[${consoleLevel}]  ${line}:${column}  ${consoleLevel}  ${message}  ${ruleId}\n`,
+				output.set(filePath, [
+					`${filePath}\n`,
+					`  ${line}:${column}  ${consoleLevel}  ${message} ${
+						ruleId ? `https://eslint.org/docs/rules/${ruleId}` : ''
+					}  ${ruleId || ''}\n`,
 				]);
 			}
 		}
 	}
-	const consoleOutput: string[] = [];
-	for (const [, v] of output) consoleOutput.push(`${v.join('')}\n`);
-	if (consoleOutput.length) console.log(consoleOutput.join(''));
+	let consoleOut = '';
+	for (const [k, v] of output) {
+		consoleOut += `${k}\n`;
+		consoleOut += `${table([v.slice(1)], { align: ['r', 'l'] })}\n`;
+	}
+	if (consoleOut) console.log(consoleOut);
 
 	return {
-		conclusion: errorCount > 0 ? 'failure' : ('success' as ChecksCreateParams['conclusion']),
-		output: {
-			title: ACTION_NAME,
-			summary: `${errorCount} error(s), ${warningCount} warning(s) found`,
-			annotations,
-		},
+		conclusion: errorCount > 0 ? 'failure' : 'success',
+		summary: `✖ ${errorCount} error(s), ${warningCount} warning(s) found`,
 	};
 }
 
 async function run() {
+	const matcher = join(__dirname, '..', '.github', 'eslint.json');
+	console.log(`##[add-matcher]${matcher}`);
 	const octokit = new GitHub(GITHUB_TOKEN!);
 
 	let currentSha: string;
@@ -137,67 +129,13 @@ async function run() {
 	}
 	debug(`Commit: ${currentSha}`);
 
-	let id: number | undefined;
-	const jobName = getInput('job-name');
-	if (jobName) {
-		try {
-			const checks = await octokit.checks.listForRef({
-				...context.repo,
-				status: 'in_progress',
-				ref: currentSha,
-			});
-			const check = checks.data.check_runs.find(({ name }) => name.toLowerCase() === jobName.toLowerCase());
-			if (check) id = check.id;
-		} catch {
-			warning("Token doesn't have permission to access this resource. Running without annotations.");
-		}
-	}
-	if (!id) {
-		try {
-			id = (await octokit.checks.create({
-				...context.repo,
-				name: ACTION_NAME,
-				head_sha: currentSha,
-				status: 'in_progress',
-				started_at: new Date().toISOString(),
-			})).data.id;
-		} catch {
-			warning("Token doesn't have permission to access this resource. Running without annotations.");
-		}
-	}
-
 	try {
 		const lintAll = getInput('lint-all');
 		const customGlob = getInput('custom-glob');
-		const { conclusion, output } = await lint(lintFiles, lintAll, customGlob);
-		if (id) {
-			try {
-				await octokit.checks.update({
-					...context.repo,
-					check_run_id: id,
-					completed_at: new Date().toISOString(),
-					conclusion,
-					output,
-				});
-			} catch {
-				warning("Token doesn't have permission to access this resource. Running without annotations.");
-			}
-		}
-		debug(output.summary);
-		if (conclusion === 'failure') setFailed(output.summary);
+		const { conclusion, summary } = await lint(lintFiles, lintAll, customGlob);
+
+		if (conclusion === 'failure') setFailed(summary);
 	} catch (e) {
-		if (id) {
-			try {
-				await octokit.checks.update({
-					...context.repo,
-					check_run_id: id,
-					conclusion: 'failure',
-					completed_at: new Date().toISOString(),
-				});
-			} catch {
-				warning("Token doesn't have permission to access this resource. Running without annotations.");
-			}
-		}
 		setFailed(e.message);
 	}
 }
